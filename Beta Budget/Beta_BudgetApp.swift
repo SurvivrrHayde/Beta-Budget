d049ead4afc44d44ce1304f5d481509795eb9dce
Matthew Haid <wsd6vn@virginia.edu>
2023-03-09 14:35:57 -0500

    Initial Commit

---
+++ Beta Budget/Beta_BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beta_BudgetApp.swift
+//  Beta Budget
+//
+//  Created by SUSAN HAID on 3/6/23.
+//
+
+import SwiftUI
+
+@main
+struct Beta_BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
